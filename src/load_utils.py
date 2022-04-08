21917d1b8b28d5b0c5d749eb234582bde50a1823
Wok <woctezuma@users.noreply.github.com>
2022-04-08 21:41:43 +0200

    Add function to load a grayscale image

--- src/load_utils.py
+++ src/load_utils.py
@@ -1,3 +1,6 @@
+import skimage
+
+
 def load_txt(fname, verbose=True):
     with open(fname, "r", encoding="utf8") as f:
         lines = f.readlines()
@@ -8,3 +11,7 @@ def load_txt(fname, verbose=True):
         print(data)
 
     return data
+
+
+def load_img(fname, as_gray=True):
+    return skimage.io.imread(fname, as_gray=as_gray)
