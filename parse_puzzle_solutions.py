88c65408ff371b0a9d896f1b42ac245954732356
Wok <woctezuma@users.noreply.github.com>
2022-04-09 15:03:05 +0200

    Convert parsed sprites to human-readable symbols

--- parse_puzzle_solutions.py
+++ parse_puzzle_solutions.py
@@ -1,7 +1,9 @@
 from glob import glob
 
-from src.data_utils import get_puzzle_name_pattern
-from src.load_utils import load_img
+from src.convert_utils import convert_all_parsed_puzzles
+from src.data_utils import get_puzzle_name_pattern, get_fname_for_character_markdown
+from src.display_utils import display_all_parsed_puzzles
+from src.load_utils import load_img, load_txt
 from src.match_utils import match_puzzle_element, match_all_puzzles
 from src.puzzle_utils import extract_puzzle_elements
 from src.sprite_utils import get_rescale_factor, load_sprites_as_img
@@ -28,6 +30,12 @@ def main(puzzle_index=0, element_index=0, debug=False):
         for fname in puzzle_fnames
     )
     all_parsed_sprites = match_all_puzzles(sprites, all_puzzles)
+
+    # Conversion of sprite indices to character names, emojis, markdown, etc.
+    symbols = load_txt(fname=get_fname_for_character_markdown())
+    converted_sprites = convert_all_parsed_puzzles(all_parsed_sprites, symbols=symbols)
+
+    display_all_parsed_puzzles(converted_sprites)
 
     return True
 
