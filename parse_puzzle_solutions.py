63e8c0e28b8ccd79386b9110fcff439cc0eba661
Wok <woctezuma@users.noreply.github.com>
2022-04-09 14:40:58 +0200

    Extract every puzzle element

--- parse_puzzle_solutions.py
+++ parse_puzzle_solutions.py
@@ -2,7 +2,7 @@ from glob import glob
 
 from src.data_utils import get_puzzle_name_pattern
 from src.load_utils import load_img
-from src.match_utils import match_puzzle_element
+from src.match_utils import match_puzzle_element, match_all_puzzles
 from src.puzzle_utils import extract_puzzle_elements
 from src.sprite_utils import get_rescale_factor, load_sprites_as_img
 
@@ -21,7 +21,12 @@ def main(puzzle_index=0, element_index=0):
     # Template matching
     sprite_index = match_puzzle_element(sprites, puzzle_element, verbose=True)
 
-
+    # Template matching for every puzzle
+    all_puzzles = (
+        extract_puzzle_elements(load_img(fname), verbose=False)
+        for fname in puzzle_fnames
+    )
+    all_parsed_sprites = match_all_puzzles(sprites, all_puzzles)
 
     return True
 
