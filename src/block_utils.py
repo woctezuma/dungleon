cd4ef7539d9a91da192bebd1be92b956c46cee03
Wok <woctezuma@users.noreply.github.com>
2022-04-09 10:06:22 +0200

    Refactoring: clarify code with a new function
    NB: the new function is extract_tiled_elements()

--- src/block_utils.py
+++ src/block_utils.py
@@ -1,3 +1,5 @@
+import math
+
 import skimage
 
 
@@ -10,5 +12,23 @@ def extract_square_blocks(img):
         block_shape = (h, h)
 
     blocks = skimage.util.view_as_blocks(img, block_shape).squeeze()
+
+    return blocks
+
+
+def extract_tiled_elements(img, element_width, element_offset):
+    image_width = img.shape[1]
+    element_stride = element_width + element_offset
+    num_elements = math.ceil(image_width / element_stride)
+
+    blocks = []
+
+    for i in range(num_elements):
+        w_start = element_stride * i
+        w_end = w_start + element_width
+
+        element = img[:, w_start:w_end]
+
+        blocks.append(element)
 
     return blocks
--- src/puzzle_utils.py
+++ src/puzzle_utils.py
@@ -1,30 +1,17 @@
-import math
-
-from src.image_utils import auto_crop
+from src.block_utils import extract_tiled_elements
+from src.image_utils import auto_crop_img_list
 
 
 def extract_puzzle_elements(img, trim_sprites=True, verbose=True):
     element_width = 128
     element_offset = 15
 
-    image_width = img.shape[1]
-    element_stride = element_width + element_offset
-    num_elements = math.ceil(image_width / element_stride)
-
-    puzzle_elements = []
-
-    for i in range(num_elements):
-        w_start = element_stride * i
-        w_end = w_start + element_width
-
-        small_element = img[:, w_start:w_end]
-
-        if trim_sprites:
-            small_element = auto_crop(small_element)
-
-        puzzle_elements.append(small_element)
+    puzzle_elements = extract_tiled_elements(img, element_width, element_offset)
 
     if verbose:
         print(f"#elements = {len(puzzle_elements)}")
+
+    if trim_sprites:
+        puzzle_elements = auto_crop_img_list(puzzle_elements)
 
     return puzzle_elements
