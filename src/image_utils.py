9b857d3ed804b0e8d5f457e7b092636e44ceca84
Wok <woctezuma@users.noreply.github.com>
2022-04-09 10:02:47 +0200

    Apply auto-crop and rescale to each image in a list

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import skimage
 from PIL import Image
+from skimage.transform import rescale
+
 
 def binarize(img):
     threshold = skimage.filters.threshold_otsu(img)
@@ -21,6 +23,26 @@ def auto_crop(img):
     pil_img = Image.fromarray(img).crop(bbox)
 
     return np.array(pil_img)
+
+
+def auto_crop_img_list(img_list):
+    cropped_img_list = []
+
+    for img in img_list:
+        cropped_img = auto_crop(img)
+        cropped_img_list.append(cropped_img)
+
+    return cropped_img_list
+
+
+def rescale_img_list(img_list, scale):
+    rescaled_img_list = []
+
+    for img in img_list:
+        rescaled_img = rescale(img, scale)
+        rescaled_img_list.append(rescaled_img)
+
+    return rescaled_img_list
 
 
 def show_image(img, max_value=255):
