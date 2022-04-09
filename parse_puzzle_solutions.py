e320998fc54e9fe53ef8eabbf1a7425e01af7962
Wok <woctezuma@users.noreply.github.com>
2022-04-09 10:11:28 +0200

    Move auto-crop inside extract_puzzle_elements()

--- parse_puzzle_solutions.py
+++ parse_puzzle_solutions.py
@@ -4,7 +4,7 @@ from src.data_utils import (
     get_solution_image_folder,
     get_fname_for_character_names,
 )
-from src.image_utils import auto_crop, show_image
+from src.image_utils import show_image
 from src.load_utils import load_txt, load_img
 from src.puzzle_utils import extract_puzzle_elements
 from src.sprite_utils import (
@@ -28,13 +28,10 @@ def main():
 
     solution_fnames = glob(f"{get_solution_image_folder()}*.jpg")
     puzzle_img = load_img(fname=solution_fnames[0], as_gray=True)
-    print(puzzle_img.shape)
 
-    trimmed_puzzle_img = auto_crop(puzzle_img)
-    print(trimmed_puzzle_img.shape)
-    show_image(trimmed_puzzle_img)
+    show_image(puzzle_img)
 
-    puzzle_elements = extract_puzzle_elements(trimmed_puzzle_img, trim_sprites=True)
+    puzzle_elements = extract_puzzle_elements(puzzle_img, trim_sprites=True)
     for e in puzzle_elements:
         print(e.shape)
 
--- src/puzzle_utils.py
+++ src/puzzle_utils.py
@@ -1,10 +1,18 @@
 from src.block_utils import extract_tiled_elements
-from src.image_utils import auto_crop_img_list
+from src.image_utils import auto_crop_img_list, auto_crop
 
 
 def extract_puzzle_elements(img, trim_sprites=True, verbose=True):
     element_width = 128
     element_offset = 15
+
+    if verbose:
+        print(f"Original image shape: {img.shape}")
+
+    img = auto_crop(img)
+
+    if verbose:
+        print(f"Auto-cropped image shape: {img.shape}")
 
     puzzle_elements = extract_tiled_elements(img, element_width, element_offset)
 
